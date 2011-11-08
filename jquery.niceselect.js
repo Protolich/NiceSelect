ff494e0c745d281ec4d486b45ce530cf1e1705a5
Joel <joel@drapper.com>
2011-11-08 10:47:54 +0000

    Initial Code

---
+++ jquery.niceselect.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	$('select.nice').css('opacity', '0').wrap('<div class="niceSelect"></div>').parent('.niceSelect').prepend('<span class="niceText"></span>');
+
+	$('select.nice').change(function (event) {
+	    $(this).parent('.niceSelect').children('span.niceText').text($(this).children('option:selected').text());
+	}).change();
+});
