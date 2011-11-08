3714fba16b6f4b1a27ad8594f5fa74044ea76b0c
Joel <joel@drapper.com>
2011-11-08 11:22:20 +0000

    Fixes #2

--- jquery.niceselect.js
+++ jquery.niceselect.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($) {
-	$('select.nice').css('opacity', '0').wrap('<div class="niceSelect"></div>').parent('.niceSelect').prepend('<span class="niceText"></span>');
+    $('select.nice').css('opacity', '0').wrap('<div class="niceSelect"></div>').parent('.niceSelect').prepend('<span class="niceText"></span>');
 
-	$('select.nice').change(function (event) {
-	    $(this).parent('.niceSelect').children('span.niceText').text($(this).children('option:selected').text());
-	}).change();
-	
-	$('select.nice').focus(function(event) {
-		$(this).parent('.niceSelect').addClass('focus');
-	}).blur(function(event) {
-		$(this).parent('.niceSelect').removeClass('focus');
-	});;
+    $('select.nice').change(function(event) {
+        $(this).parent('.niceSelect').children('span.niceText').text($(this).children('option:selected').text());
+    }).change();
+
+    $('select.nice').focus(function(event) {
+        $(this).parent('.niceSelect').addClass('focus');
+    }).blur(function(event) {
+        $(this).parent('.niceSelect').removeClass('focus');
+    });;
 });
