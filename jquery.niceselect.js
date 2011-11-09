eea536541c102f3db5e31310d10a82204461424c
Joel <joel@drapper.com>
2011-11-09 10:52:20 +0000

    Fixes #4
    Added a bunch of comments to the code.

--- jquery.niceselect.js
+++ jquery.niceselect.js
@@ -13,17 +13,21 @@
 
 		// iterate over matched elements
         return this.each(function() {
-	
+			
+			// Wraps the element in a div which can be styled to look like the select element
             $(this).css('opacity', '0').wrap('<div class="niceSelect"></div>').parent('.niceSelect').prepend('<span class="niceText"></span>');
-
+			
+			// Changes the text in the fake select element to reflect the changes to the real select element
 			$(this).change(function(event) {
 				$(this).parent('.niceSelect').children('span.niceText').text($(this).children('option:selected').text());
 			}).change();
 			
+			// Adds focus class to wrapper when the select is focused
 			$(this).focus(function(event) {
 				$(this).parent('.niceSelect').addClass('focus');
 			});
 			
+			// Removes focus class on blur
 			$(this).blur(function(event) {
 				$(this).parent('.niceSelect').removeClass('focus');
 			});
