7a396c955dd60e81acbdbfd840dc76541ebb94dd
Thomas Stradling <protolich@gmail.com>
2014-03-24 21:04:48 +0000

    Updated a class when a default value is selected. Default value can be assigned as an option.

--- jquery.niceselect.js
+++ jquery.niceselect.js
@@ -1,9 +1,12 @@
 (function ($) {
+	"use strict";
 	// replace 'pluginName' with the name of your plugin
 	$.fn.niceSelect = function (options) {
 
 		// plugin default options
-		var defaults = {};
+		var defaults = {
+			default: ''
+		};
 
 		// extends defaults with options provided
 		if (options) {
@@ -12,12 +15,14 @@
 
 		// iterate over matched elements
 		return this.each(function () {
-			var _this,
+			var select = this,
+				_this,
 				_wrapper,
 				_text,
 				text;
 
 			_this = $(this);
+
 			// Wraps the element in a div which can be styled to look like the select element
 			_this
 				.addClass('niceselect-activated')
@@ -28,14 +33,22 @@
 			_wrapper = _this.parent('.niceselect-wrapper');
 			_text = $('.niceselect-text', _wrapper);
 
-			// Changes the text in the fake select element to reflect the changes to the real select element
-			_this.on('change', function (event) {
+			// Set the label and any classes
+			var niceselectUpdate = function () {
+				if (select.value === defaults.default) {
+					_wrapper.addClass('niceselect-default');
+				} else {
+					_wrapper.removeClass('niceselect-default');
+				}
 				text = _this.find('option:selected').text();
 				_text.text(text);
-			});
+			};
 
-			text = _this.find('option:selected').text();
-			_text.text(text);
+			// Changes the text in the fake select element to reflect the changes to the real select element
+			_this.on('change', niceselectUpdate);
+
+			// Set the initial text instead of triggering the change event
+			niceselectUpdate();
 
 			// Adds focus class to wrapper when the select is focused
 			_this.on('focus', function (event) {
